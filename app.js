1f61f9ad6cef2c49e7c984a1a0319dab8c9599b2
Jonathan WALLER <public@jonwaller.net>
2013-01-05 02:30:16 +0900

    Using hbs. Sheep added.

--- app.js
+++ app.js
@@ -2,33 +2,32 @@ var express = require('express');
 var app = module.exports = express.createServer();
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'hbs');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Home'
-  });
+	res.render('index', {});
+});
+
+app.get('/categories', function(req, res){
+	res.render('categories', {});
+});
+
+app.get('/products', function(req, res){
+	res.render('products', {});
 });
 
 app.get('/about', function(req, res){
-  res.render('about', {
-    title: 'About'
-  });
+	res.render('about', {});
 });
 
-app.get('/contact', function(req, res){
-  res.render('contact', {
-    title: 'Contact'
-  });
-});
-
+console.log("Gekiyasu server starting...");
 app.listen(8080);
 console.log("Listening on port %d", app.address().port);
